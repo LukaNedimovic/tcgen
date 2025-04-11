0ef995da5818fe5ed00ad6bd4c093cc76bb82b91
Luka Nedimović <lukazen123@gmail.com>
2025-04-11 02:39:09 +0200

    feat: ✨ Add pre-commit hooks

--- setup.sh
+++ setup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# This script will create / load virtual environment, and load the dependencies
+
+if [ ! -d "venv" ]; then
+  python3 -m venv venv
+fi
+source venv/bin/activate
+
+pip install --upgrade pip
+pip install -e .
 pip install -r requirements.txt
+
+pre-commit install
+
+echo "✅ Dev environment is ready!"
