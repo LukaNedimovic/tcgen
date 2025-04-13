2ea54e79f9f8e5682757a474e5ae7b916f5cfd16
Luka Nedimović <lukazen123@gmail.com>
2025-04-13 14:23:35 +0200

    chore: ⚙️ Update `setup.sh` (add logging)

--- setup.sh
+++ setup.sh
@@ -2,14 +2,24 @@
 
 # This script will create / load virtual environment, and load the dependencies
 
+
 if [ ! -d "venv" ]; then
+  echo "⏳ Creating venv..."
   python3 -m venv venv
+  echo "✅ Venv created!"
 fi
 source venv/bin/activate
+echo "✅ Sucessfully activated environment!"
 
 pip install --upgrade pip
+
+echo "⏳ Installing module in editable mode..."
 pip install -e .
+echo "✅ Module successfully installed!"
+
+echo "⏳ Installing requirements..."
 pip install -r requirements.txt
+echo "✅ Requirements succesfully installed!"
 
 pre-commit install
 
